98f9bb17df6c24e3739545216f40c022b5d22b34
Utkarsh Varma <utkarshvarma.dev@gmail.com>
2024-01-06 16:08:41 -0500

    kafka integration completed

--- ucraft/kafka.py
+++ ucraft/kafka.py
@@ -2,12 +2,22 @@ from confluent_kafka import Producer
 import datetime
 import json
 
+import frappe
+
 
 def send_to_kafka(doc, method):
-    # Convert datetime objects to strings
-    data = {k: str(v) if isinstance(v, datetime.datetime) else v for k, v in doc.as_dict().items()}
-    data = json.dumps(data)
-    key = get_kafka_key(doc, method)
+    kafka_config = frappe.get_doc(
+        "Ucraft Kafka Configuration",
+    )
+    should_execute, producer, topic = kafka_config.create_kafka_producer()
+    if should_execute:
+
+        data = {k: str(v) if isinstance(v, datetime.datetime) else v for k, v in doc.as_dict().items()}
+        data = json.dumps(data)
+        key = get_kafka_key(doc, method)
+        producer.produce(topic, key=key, value=data)
+        print(f"Sent {data} to Kafka topic {topic} with key {key}")
+        producer.flush()
 
 
 def get_kafka_key(doc, method):
--- ucraft/ucraft/doctype/ucraft_kafka_configuration/ucraft_kafka_configuration.py
+++ ucraft/ucraft/doctype/ucraft_kafka_configuration/ucraft_kafka_configuration.py
@@ -3,7 +3,30 @@
 
 # import frappe
 from frappe.model.document import Document
+from confluent_kafka import Producer
 
 
 class UcraftKafkaConfiguration(Document):
-	pass
+
+    def create_kafka_producer(self):
+        # Extract the configuration details from the current instance
+        if self.disable_sync_to_kafka:
+            return False, None, None
+
+        kafka_bootstrap_service = self.kafka_bootstrap_service
+        kafka_client_id = self.kafka_client_id
+        kafka_sasl_username = self.kafka_sasl_username
+        kafka_sasl_password = self.kafka_sasl_password
+        kafks_security_protocol = self.kafks_security_protocol
+        kafka_topic = self.kafka_topic
+
+        # Create the Kafka producer
+        p = Producer({
+            'bootstrap.servers': kafka_bootstrap_service,
+            'client.id': kafka_client_id,
+            'sasl.username': kafka_sasl_username,
+            'sasl.password': kafka_sasl_password,
+            'security.protocol': kafks_security_protocol,
+        })
+
+        return True, p, kafka_topic
